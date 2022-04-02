592f260eba857e94076c3f07453497bc6ec5f11a
Michael S <test@test.com>
2022-04-02 12:05:57 +0300

    Fields rearranged and renamed

--- igrf/result.go
+++ igrf/result.go
@@ -1,32 +1,35 @@
 package igrf
 
+// IGRFresults represents the result of IGRF calculation
+//
+// Fields:
+// Declination (D):  14.175 ° (+ve east)
+// DeclinationSV (D): -8.92 arcmin/yr
+// Inclination (I):  74.229 ° (+ve down)
+// InclinationSV (I): -2.59 arcmin/yr
+// HzIntensity (Horizontal intensity (H)):  14626.6 nT
+// HorizontalSV (H):  -16.8 nT/yr
+// NorthComponent (X):  14181.2 nT
+// NorthSV (X): -28.2 nT/yr
+// EastComponent (Y):  3581.8 nT
+// EastSV (Y):  32.0 nT/yr
+// VerticalComponent (Z):  51788.4 nT
+// VerticalSV (Z):  80.1 nT/yr
+// TotalIntensity (F):  53814.3 nT
+// TotalSV (F):  71.8 nT/yr
 type IGRFresults struct {
-	// Declination (D):  14.175 °
-	Declination float32
-	// Inclination (I):  74.229 °
-	Inclination float32
-	// Horizontal intensity (H):  14626.6 nT
-	HzIntensity float32
-	// Total intensity (F):  53814.3 nT
-	TotalIntensity float32
-	// North component (X):  14181.2 nT
-	NorthComponent float32
-	// East component (Y):  3581.8 nT
-	EastComponent float32
-	// Vertical component (Z):  51788.4 nT
-	VerticalComponent float32
-	// Declination SV (D): -8.92 arcmin/yr
-	DeclinationSV float32
-	// Inclination SV (I): -2.59 arcmin/yr
-	InclinationSV float32
-	// Horizontal SV (H):  -16.8 nT/yr
-	HorizontalSV float32
-	// Total SV (F):  71.8 nT/yr
-	TotalSV float32
-	// North SV (X): -28.2 nT/yr
-	NorthSV float32
-	// East SV (Y):  32.0 nT/yr
-	EastSV float32
-	// Vertical SV (Z):  80.1 nT/yr
-	VerticalSV float32
+	Declination         float32
+	DeclinationSV       float32
+	Inclination         float32
+	InclinationSV       float32
+	HorizontalIntensity float32
+	HorizontalSV        float32
+	NorthComponent      float32
+	NorthSV             float32
+	EastComponent       float32
+	EastSV              float32
+	VerticalComponent   float32
+	VerticalSV          float32
+	TotalIntensity      float32
+	TotalSV             float32
 }
