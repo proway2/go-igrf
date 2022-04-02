fe8808f61d3d27a1cd0a899c598ee5991ea59be5
Michael S <test@test.com>
2022-04-02 10:58:50 +0300

    - Added `error` into function's signature. - Added annotation.

--- igrf/igrf.go
+++ igrf/igrf.go
@@ -1,7 +1,11 @@
 package igrf
 
-func IGRF(lat, lon, alt, date float32) IGRFresults {
-	// lat, lon - geodetic (shape of Earth using the WGS-84 ellipsoid)
+// IGRF computes values for the geomagnetic field and secular variation for a given set of coordinates and date.
+// lat, lon - geodetic latitude and longitude (WGS84 latitude and altitude above mean sea level).
+// Valid values -90.0 < lat < 90.0, -180.0 < lon < 180.0.
+// alt - geodetic altitude above mean sea level in km (-1.00 to 600.00).
+// date - decimal date (1900.00 to 2025).
+func IGRF(lat, lon, alt, date float32) (IGRFresults, error) {
 	res := IGRFresults{}
-	return res
+	return res, nil
 }
--- igrf/igrf_test.go
+++ igrf/igrf_test.go
@@ -13,9 +13,10 @@ func TestIGRF(t *testing.T) {
 		date float32
 	}
 	tests := []struct {
-		name string
-		args args
-		want IGRFresults
+		name    string
+		args    args
+		want    IGRFresults
+		wantErr bool
 	}{
 		{
 			name: "lat: 59.9, lon: 39.9, alt: 0.0, date: 2019.123",
@@ -28,19 +29,25 @@ func TestIGRF(t *testing.T) {
 				NorthComponent:    14204.1,
 				EastComponent:     3550.1,
 				VerticalComponent: 51729.1,
-				DeclinationSV:     -9.55,
-				InclinationSV:     -2.22,
-				HorizontalSV:      -16.8,
-				TotalSV:           59.5,
+				DeclinationSV:     9.6,
+				InclinationSV:     2.2,
+				HorizontalSV:      -16.3,
+				TotalSV:           59.8,
 				NorthSV:           -25.8,
 				EastSV:            35.9,
 				VerticalSV:        66.7,
 			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := IGRF(tt.args.lat, tt.args.lon, tt.args.alt, tt.args.date); !reflect.DeepEqual(got, tt.want) {
+			got, err := IGRF(tt.args.lat, tt.args.lon, tt.args.alt, tt.args.date)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("IGRF() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("IGRF() = %v, want %v", got, tt.want)
 			}
 		})
