aab3f9de074f1cd2711995acbc4537f131142c7e
Michael S <test@test.com>
2022-04-05 21:35:37 +0300

    WIP: reading coeffs

--- coeffs/read.go
+++ coeffs/read.go
@@ -2,6 +2,7 @@ package coeffs
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -11,24 +12,58 @@ import (
 )
 
 const file_name string = "../coeffs/igrf13coeffs.txt"
+const coeffs_lines = 195
+const years_before_sv = 5
 
 var space_re *regexp.Regexp = regexp.MustCompile(`\s+`)
 var year_re *regexp.Regexp = regexp.MustCompile(`\d{4}`)
 var year_sv_re *regexp.Regexp = regexp.MustCompile(`\d{4}-\d{2}`)
 
 type IGRFcoeffs struct {
-	time   []float64
-	coeffs [][]float64
+	names  *[]string
+	epochs *[]float64
+	coeffs *[]lineData
 }
 
-func LoadCoeffsFile() {
+type lineData struct {
+	g_h    bool // the model contains either "g" or "h" g_h == true if there is "g" (false if "h")
+	deg_n  int
+	ord_m  int
+	coeffs *[]float64
+}
+
+func NewCoeffsData() *IGRFcoeffs {
+	igrf := IGRFcoeffs{}
+	igrf.readCoeffs()
+	return &igrf
+}
+
+func Coeffs(date float64) (*[]float64, error) {
+	return &[]float64{}, nil
+}
+
+func (igrf *IGRFcoeffs) readCoeffs() {
 	f, err := os.Open(file_name)
 	defer f.Close()
 	if err != nil {
-		panic("IGRF Coeffs file not found.")
+		log.Fatal("IGRF Coeffs file not found.")
 	}
-	cs_re := regexp.MustCompile(`^c/s.*`)
 	scanner := bufio.NewScanner(f)
+	names, epochs, err := getEpochs(scanner)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	igrf.names = names
+	igrf.epochs = epochs
+	coeffs, err := getCoeffs(scanner)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	igrf.coeffs = coeffs
+}
+
+func getEpochs(scanner *bufio.Scanner) (*[]string, *[]float64, error) {
+	cs_re := regexp.MustCompile(`^c/s.*`)
 	for scanner.Scan() {
 		line := scanner.Text()
 		line = strings.Trim(line, " ")
@@ -38,14 +73,11 @@ func LoadCoeffsFile() {
 		if cs_re.Match([]byte(line)) {
 			scanner.Scan()
 			line2 := scanner.Text()
-			a, b := parseHeader1(line, line2)
-			fmt.Println(a, b)
+			names, epochs := parseHeader1(line, line2)
+			return &names, &epochs, nil
 		}
 	}
-}
-
-func getEpochs(line string) []float64 {
-	return []float64{}
+	return nil, nil, errors.New("Unable to get epochs.")
 }
 
 func parseHeader1(line1, line2 string) ([]string, []float64) {
@@ -80,4 +112,46 @@ func parseHeader1(line1, line2 string) ([]string, []float64) {
 		}
 	}
 	return names, epochs[shift:]
+}
+
+func getCoeffs(scanner *bufio.Scanner) (*[]lineData, error) {
+	coeffs := make([]lineData, coeffs_lines)
+	for i := 0; scanner.Scan(); i++ {
+		data := lineData{}
+		line := scanner.Text()
+		line = strings.Trim(line, " ")
+		line_data := space_re.Split(line, -1)
+		if line_data[0] == "g" {
+			data.g_h = true
+		} else {
+			data.g_h = false
+		}
+		deg, _ := strconv.ParseInt(line_data[1], 10, 0)
+		data.deg_n = int(deg)
+		ord, _ := strconv.ParseInt(line_data[2], 10, 0)
+		data.ord_m = int(ord)
+		line_coeffs, err := parseArrayToFloat(line_data[3:])
+		if err != nil {
+			return nil, errors.New("Unable to parse coeffs.")
+		}
+		data.coeffs = line_coeffs
+		coeffs[i] = data
+	}
+	return &coeffs, nil
+}
+
+func parseArrayToFloat(raw_data []string) (*[]float64, error) {
+	data := make([]float64, len(raw_data))
+	for index, token := range raw_data {
+		real_data, err := strconv.ParseFloat(token, 32)
+		if err != nil {
+			return nil, errors.New("Unable to parse coeffs.")
+		}
+		if index == len(raw_data)-1 {
+			real_data = data[index-1] + real_data*years_before_sv
+			fmt.Println(real_data)
+		}
+		data[index] = real_data
+	}
+	return &data, nil
 }
